3c7cb9a8365c4295aeffc115f3c7f6d4c20ebc4a
Mark Fisher <mfisher36@gmail.com>
2018-11-28 07:18:51 -0500

    saving work

--- public/js/script.js
+++ public/js/script.js
@@ -1,163 +1,332 @@
-console.log("Hello World")
+//variable for url to get team name
+const url = location.href.split("/");
+const team = url[url.length - 1]
+const teamData = {
+  cardinals: {
+    primary: "#97233F",
+    secondary: "#000000",
+    city: "arizona",
+    name: "cardinals"
+  },
 
-const teamCSS = {
- cardinals: {
-  primary: "#97233F;",
-  secondary: "#000000;"
- },
+  falcons: {
+    primary: "#A71930",
+    secondary: "#000000",
+    city: "atlanta",
+    name: "falcons"
+  },
 
-falcons: {
-  primary: "#A71930;",
-  secondary: "#000000;"
- },
+  ravens: {
+    primary: "#241773",
+    secondary: "#000000",
+    city: "baltimore",
+    name: "ravens"
+  },
 
-ravens: {
-  primary: "#241773;",
-  secondary: "#000000;"
- },
+  bills: {
+    primary: "#00338D",
+    secondary: "#C60C30",
+    city: "buffalo",
+    name: "bills"
+  },
 
-bills: {
-  primary: "#00338D;",
-  secondary: "#C60C30;"
- },
+  panthers: {
+    primary: "#0085CA",
+    secondary: "#101820",
+    city: "carolina",
+    name: "panthers"
+  },
 
-panthers: {
-  primary: "#0085CA;",
-  secondary: "#101820;"
- },
+  bears: {
+    primary: "#0B162A",
+    secondary: "#C83803",
+    city: "chicago",
+    name: "bears"
+  },
 
-bears: {
-  primary: "#0B162A;",
-  secondary: "#C83803;"
- },
+  bengals: {
+    primary: "#FB4F14",
+    secondary: "#000000",
+    city: "cincinnati",
+    name: "bengals"
+  },
 
-bengals: {
-  primary: "#FB4F14;",
-  secondary: "#000000;"
- },
+  browns: {
+    primary: "#311D00",
+    secondary: "#FF3C00",
+    city: "cleveland",
+    name: "browns"
+  },
 
-browns: {
-  primary: "#311D00;",
-  secondary: "#FF3C00;"
- },
+  cowboys: {
+    primary: "#003594",
+    secondary: "#041E42",
+    city: "dallas",
+    name: "cowboys"
+  },
 
-cowboys: {
-  primary: "#003594;",
-  secondary: "#041E42;"
- },
+  broncos: {
+    primary: "#FB4F14",
+    secondary: "#002244",
+    city: "denver",
+    name: "broncos"
+  },
 
-broncos: {
-  primary: "#FB4F14;",
-  secondary: "#002244;"
- },
+  lions: {
+    primary: "#0076B6",
+    secondary: "#B0B7BC",
+    city: "detroit",
+    name: "lions"
+  },
 
-lions: {
-  primary: "#0076B6;",
-  secondary: "#B0B7BC;"
- },
+  packers: {
+    primary: "#203731",
+    secondary: "#FFB612",
+    city: "green bay",
+    name: "packers"
+  },
 
-packers: {
-  primary: "#203731;",
-  secondary: "#FFB612;"
- },
+  texans: {
+    primary: "#03202F",
+    secondary: "#A71930",
+    city: "houston",
+    name: "texans"
+  },
 
-texans: {
-  primary: "#03202F;",
-  secondary: "#A71930;"
- },
+  colts: {
+    primary: "#002C5F",
+    secondary: "#A2AAAD",
+    city: "indianapolis",
+    name: "colts"
+  },
 
-colts: {
-  primary: "#002C5F;",
-  secondary: "#A2AAAD;"
- },
+  jaguars: {
+    primary: "#101820",
+    secondary: "#D7A22A",
+    city: "jacksonville",
+    name: "jaguars"
+  },
 
-jaguars: {
-  primary: "#101820;",
-  secondary: "#D7A22A;"
- },
+  chiefs: {
+    primary: "#E31837",
+    secondary: "#FFB81C",
+    city: "kansas city",
+    name: "chiefs"
+  },
 
-chiefs: {
-  primary: "#E31837;",
-  secondary: "#FFB81C;"
- },
+  chargers: {
+    primary: "#002A5E",
+    secondary: "#FFC20E",
+    city: "los angeles",
+    name: "chargers"
+  },
 
-chargers: {
-  primary: "#002A5E;",
-  secondary: "#FFC20E;"
- },
+  rams: {
+    primary: "#002244",
+    secondary: "#866D4B",
+    city: "los angeles",
+    name: "rams"
+  },
 
-rams: {
-  primary: "#002244;",
-  secondary: "#866D4B;"
- },
+  dolphins: {
+    primary: "#008E97",
+    secondary: "#F26A24",
+    city: "miami",
+    name: "dolphins"
+  },
 
-dolphins: {
-  primary: "#008E97;",
-  secondary: "#F26A24;"
- },
+  vikings: {
+    primary: "#4F2683",
+    secondary: "#FFC62F",
+    city: "minnesota",
+    name: "vikings"
+  },
 
-vikings: {
-  primary: "#4F2683;",
-  secondary: "#FFC62F;"
- },
+  patriots: {
+    primary: "#002244",
+    secondary: "#C60C30",
+    city: "new england",
+    name: "patriots"
+  },
 
-patriots: {
-  primary: "#002244;",
-  secondary: "#C60C30;"
- },
+  saints: {
+    primary: "#D3BC8D",
+    secondary: "#101820",
+    city: "new orleans",
+    name: "saints"
+  },
 
-saints: {
-  primary: "#D3BC8D;",
-  secondary: "#101820;"
- },
+  giants: {
+    primary: "#0B2265",
+    secondary: "#A71930",
+    city: "new york",
+    name: "giants"
+  },
 
-giants: {
-  primary: "#0B2265;",
-  secondary: "#A71930;"
- },
+  jets: {
+    primary: "#003F2D",
+    secondary: "#FFFFFF",
+    city: "new york",
+    name: "jets"
+  },
 
-jets: {
-  primary: "#003F2D;",
-  secondary: "#FFFFFF;"
- },
+  raiders: {
+    primary: "#000000",
+    secondary: "#A5ACAF",
+    city: "oakland",
+    name: "raiders"
+  },
 
-raiders: {
-  primary: "#000000;",
-  secondary: "#A5ACAF;"
- },
+  eagles: {
+    primary: "#004C54",
+    secondary: "#A5ACAF",
+    city: "philadelphia",
+    name: "eagles"
+  },
 
-eagles: {
-  primary: "#004C54;",
-  secondary: "#A5ACAF;"
- },
+  steelers: {
+    primary: "#FFB612",
+    secondary: "#101820",
+    city: "pittsburgh",
+    name: "steelers"
+  },
 
-steelers: {
-  primary: "#FFB612;",
-  secondary: "#101820;"
- },
+  niners: {
+    primary: "#AA0000",
+    secondary: "#B3995D",
+    city: "san francisco",
+    name: "49ers"
+  },
 
-niners: {
-  primary: "#AA0000;",
-  secondary: "#B3995D;"
- },
+  seahawks: {
+    primary: "#002244",
+    secondary: "#69BE28",
+    city: "seattle",
+    name: "seahawks"
+  },
 
-seahawks: {
-  primary: "#002244;",
-  secondary: "#69BE28;"
- },
+  buccaneers: {
+    primary: "#D50A0A",
+    secondary: "#FF7900",
+    city: "tampa bay",
+    name: "buccaneers"
+  },
 
-buccaneers: {
-  primary: "#D50A0A;",
-  secondary: "#FF7900;"
- },
+  titans: {
+    primary: "#002A5C",
+    secondary: "#4495D1",
+    city: "tennesee",
+    name: "titans"
+  },
 
-titans: {
-  primary: "#002A5C;",
-  secondary: "#4495D1;"
- },
-
-redskins: {
-  primary: "#773141;",
-  secondary: "#FFB612;"
- }
+  redskins: {
+    primary: "#773141",
+    secondary: "#FFB612",
+    city: "washington",
+    name: "redskins"
+  }
 }
+
+
+
+
+console.log("Hello World")
+console.log(team)
+
+// const rootStyles = getComputedStyle(root);
+// const primaryColor = rootStyles.getPropertyValue('--primary-color');
+// const secondaryColor = rootStyles.getPropertyValue('--secondary-color');
+
+
+
+
+
+
+
+
+
+
+
+
+const setTeamPage = () => {
+
+
+  //variable for the root class set for CSS Variables
+  const root = document.querySelector(':root');
+
+  //assign and set primary and secondary colors from teamData object based on team
+  const $primaryColor = teamData[team].primary;
+  const $secondaryColor = teamData[team].secondary;
+  root.style.setProperty('--primary-color', $primaryColor)
+  root.style.setProperty('--secondary-color', $secondaryColor)
+
+  //changing image based on team
+  const $jumboImage = `/photos/$${team}.png`
+  $(".jumbo-image").attr("src", $jumboImage);
+
+  $("#teamCity").text(teamData[team].city.toUpperCase())
+  $("#teamName").text(teamData[team].name.toUpperCase())
+}
+setTeamPage();
+
+
+
+
+const setTeamNews = () => {
+
+  const teamName = teamData[team].city + " " + teamData[team].name;
+  $.ajax({
+    url: "/api/news/" + teamName,
+    method: "GET"
+  }).then(function (data) {
+
+    const teamNewsArray = [];
+
+    data.articles.forEach((article) => {
+      if (article.title.includes(teamData[team].name.charAt(0).toUpperCase()) && article.urlToImage) {
+        teamNewsArray.push(
+          {
+            "title": article.title,
+            "url": article.url,
+            "imgUrl":article.urlToImage
+          }
+        )
+        
+
+
+    // if ($(".article-pic-left")[index]) {
+
+    //   $(".article-pic-left")[index].src = article.urlToImage;
+    // }
+
+    // const articleData = [article.title, article.url, article.urlToImage];
+
+  }
+});
+console.log(teamNewsArray)
+
+console.log($(".text"))
+
+        for (let x = 0; x < $(".article-pic-left").length; x++) {
+          $(".article-pic-left")[x].src = teamNewsArray[x].imgUrl;
+          $(".text")[x].innerText = teamNewsArray[x].title;
+        }
+
+
+
+
+
+  })
+}
+
+
+setTeamNews();
+
+
+
+
+
+
+
+
+// console.log($(".article-pic-left")[0])
--- server.js
+++ server.js
@@ -1,53 +1,122 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('4064b95643d24a48a9b28a7ad95f81e4');
 const fs = require('fs');
-// var MySportsFeeds = require("mysportsfeeds-node");
-// var msf = new MySportsFeeds("2.0", true);
+var MySportsFeeds = require("mysportsfeeds-node");
+var msf = new MySportsFeeds("2.0", true);
 // msf.authenticate("3c05ee98-ad49-4e16-b24e-46c9b5", "MYSPORTSFEEDS");
 // var data = msf.getData('nfl', '2018-2019-regular', 'players', 'json', {team: "NYJ", rosterstatus: "assigned-to-roster", position: "qb"});
+// var data = msf.getData('nfl', '2018-2019-regular', 'weekly_games', 'json', {week: "12", sort: "game.starttime", rosterstatus: "assigned-to-roster", force: "true"});
+
+
+const express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+app.get("/team/:team", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/team.html"));
+  // res.sendFile(path.join(__dirname, "./public/css/team.style.css"));
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/login.html"));
+});
+
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/login.html"));
+});
+
+app.get("/signup", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/signup.html"));
+});
+
+
+app.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/home.html"));
+});
+
+app.get("/fanchat", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/fanchat.html"));
+});
+
+app.get("/fanposts", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/fanposts.html"));
+});
+
 
 // Reads the file that gets created with the data called from the API
-fs.readFile('results/players-nfl-2018-2019-regular.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  JSON.parse(data, null, 2).players.forEach((player, i) => {
-    const playerData = [player.player.jerseyNumber, player.player.firstName, player.player.lastName, player.player.primaryPosition, player.player.height, player.player.weight, player.player.age];
-    playerData.forEach(player => console.log(isNullOrZero(player)));
+// fs.readFile('results/players-nfl-2018-2019-regular.json', 'utf8', function (err, data) {
+//   if (err) throw err;
+//   JSON.parse(data, null, 2).players.forEach((player, i) => {
+//     const playerData = [player.player.jerseyNumber, player.player.firstName, player.player.lastName, player.player.primaryPosition, player.player.height, player.player.weight, player.player.age];
+//     playerData.forEach(player => console.log(isNullOrZero(player)));
+//   })
+// });
+
+
+
+
+// fs.readFile('results/weekly_games-nfl-2018-2019-regular.json', 'utf8', function (err, data) {
+//   if (err) throw err;
+//   JSON.parse(data, null, 2).games.forEach((game, i) => {
+//     const gameData = [game.schedule.week];
+//     gameData.forEach(game => console.log(isNullOrZero(game)));
+//   })
+// });
+
+
+app.get("/api/news/:team", function (req, res) {
+console.log(req.params.team)
+  const teamName = (req.params.team.split(" ")[1]);
+
+  // Team News API Call
+  newsapi.v2.everything({
+    q: req.params.team,
+    // q: 'philadelphia eagles',
+    pageSize: 50,
+    sortBy: 'publishedAt',
+    language: 'en'
+  }).then((response, err) => {
+    if (err) throw err;
+
+    
+
+    
+    
+    res.json(response)
+
   })
-});
-
-// NFL News API Call
-newsapi.v2.everything({
-  q: 'philadelphia eagles',
-  pageSize: 20,
-  sortBy: 'publishedAt',
-  language: 'en'
-}).then((response, err) => {
-  if (err) throw err;
-
-  response.articles.forEach(article => {
-    const articleData = [article.title, article.url, article.urlToImage];
-    if (article.title.includes("Eagles")) {
-      articleData.forEach(article => console.log(article))
-    }
-  });
 })
 
-// Team News API Call
-newsapi.v2.everything({
-  sources: "nfl-news",
-  sortBy: 'publishedAt',
-  pageSize: 7,
-  language: 'en'
-}).then((response, err) => {
-  if (err) throw err;
 
-  response.articles.forEach(article => {
-    const nflArticleData = [article.source.name, article.publishedAt, article.url, article.urlToImage, article.title, article.description]
-    nflArticleData.forEach(article => console.log(article))
-  })
-});
+
+  // NFL News API Call
+  // newsapi.v2.everything({
+  //   sources: "nfl-news",
+  //   sortBy: 'publishedAt',
+  //   pageSize: 7,
+  //   language: 'en'
+  // }).then((response, err) => {
+  //   if (err) throw err;
+
+  //   response.articles.forEach(article => {
+  //     const nflArticleData = [article.source.name, article.publishedAt, article.url, article.urlToImage, article.title, article.description]
+  //     nflArticleData.forEach(article => console.log(article))
+  //   })
+  // });
+
 
 // Function to replace data in Roster Chart not put in by API with a '*'. Will have a note on the site for this
 function isNullOrZero(data) {
   return (data === null || data === 0) ? ('*') : (data);
 }
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
