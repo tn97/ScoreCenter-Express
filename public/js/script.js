5f5be47aeb8eaa257b4bbef043d28d9f7608ffe0
Mark Fisher <mfisher36@gmail.com>
2018-11-28 16:00:02 -0500

    saving work

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,7 @@ const teamData = {
     name: "falcons",
     abbr: "ATL"
   },
-  
+
   ravens: {
     primary: "#241773",
     secondary: "#000000",
@@ -26,7 +26,7 @@ const teamData = {
     name: "ravens",
     abbr: "BAL"
   },
-  
+
   bills: {
     primary: "#00338D",
     secondary: "#C60C30",
@@ -34,7 +34,7 @@ const teamData = {
     name: "bills",
     abbr: "BUF"
   },
-  
+
   panthers: {
     primary: "#0085CA",
     secondary: "#101820",
@@ -42,7 +42,7 @@ const teamData = {
     name: "panthers",
     abbr: "CAR"
   },
-  
+
   bears: {
     primary: "#0B162A",
     secondary: "#C83803",
@@ -50,7 +50,7 @@ const teamData = {
     name: "bears",
     abbr: "CHI"
   },
-  
+
   bengals: {
     primary: "#FB4F14",
     secondary: "#000000",
@@ -58,7 +58,7 @@ const teamData = {
     name: "bengals",
     abbr: "CIN"
   },
-  
+
   browns: {
     primary: "#311D00",
     secondary: "#FF3C00",
@@ -66,7 +66,7 @@ const teamData = {
     name: "browns",
     abbr: "CLE"
   },
-  
+
   cowboys: {
     primary: "#003594",
     secondary: "#041E42",
@@ -74,7 +74,7 @@ const teamData = {
     name: "cowboys",
     abbr: "DAL"
   },
-  
+
   broncos: {
     primary: "#FB4F14",
     secondary: "#002244",
@@ -82,7 +82,7 @@ const teamData = {
     name: "broncos",
     abbr: "DEN"
   },
-  
+
   lions: {
     primary: "#0076B6",
     secondary: "#B0B7BC",
@@ -90,7 +90,7 @@ const teamData = {
     name: "lions",
     abbr: "DET"
   },
-  
+
   packers: {
     primary: "#203731",
     secondary: "#FFB612",
@@ -98,7 +98,7 @@ const teamData = {
     name: "packers",
     abbr: "GB"
   },
-  
+
   texans: {
     primary: "#03202F",
     secondary: "#A71930",
@@ -106,7 +106,7 @@ const teamData = {
     name: "texans",
     abbr: "HOU"
   },
-  
+
   colts: {
     primary: "#002C5F",
     secondary: "#A2AAAD",
@@ -114,7 +114,7 @@ const teamData = {
     name: "colts",
     abbr: "IND"
   },
-  
+
   jaguars: {
     primary: "#101820",
     secondary: "#D7A22A",
@@ -122,7 +122,7 @@ const teamData = {
     name: "jaguars",
     abbr: "JAX"
   },
-  
+
   chiefs: {
     primary: "#E31837",
     secondary: "#FFB81C",
@@ -130,7 +130,7 @@ const teamData = {
     name: "chiefs",
     abbr: "KC"
   },
-  
+
   chargers: {
     primary: "#002A5E",
     secondary: "#FFC20E",
@@ -138,7 +138,7 @@ const teamData = {
     name: "chargers",
     abbr: "LAC"
   },
-  
+
   rams: {
     primary: "#002244",
     secondary: "#866D4B",
@@ -146,7 +146,7 @@ const teamData = {
     name: "rams",
     abbr: "LA"
   },
-  
+
   dolphins: {
     primary: "#008E97",
     secondary: "#F26A24",
@@ -154,7 +154,7 @@ const teamData = {
     name: "dolphins",
     abbr: "MIA"
   },
-  
+
   vikings: {
     primary: "#4F2683",
     secondary: "#FFC62F",
@@ -162,7 +162,7 @@ const teamData = {
     name: "vikings",
     abbr: "MIN"
   },
-  
+
   patriots: {
     primary: "#002244",
     secondary: "#C60C30",
@@ -170,7 +170,7 @@ const teamData = {
     name: "patriots",
     abbr: "NE"
   },
-  
+
   saints: {
     primary: "#D3BC8D",
     secondary: "#101820",
@@ -178,7 +178,7 @@ const teamData = {
     name: "saints",
     abbr: "NO"
   },
-  
+
   giants: {
     primary: "#0B2265",
     secondary: "#A71930",
@@ -186,7 +186,7 @@ const teamData = {
     name: "giants",
     abbr: "NYG"
   },
- 
+
   jets: {
     primary: "#003F2D",
     secondary: "#FFFFFF",
@@ -311,7 +311,7 @@ const teamData = {
 //     const teamNewsArray = [];
 
 //     filteredArticles.forEach((article) => {
-      
+
 //         teamNewsArray.push(
 //           {
 //             "title": article.title,
@@ -321,8 +321,8 @@ const teamData = {
 //             "description": article.description
 //           });
 //         })
-        
-      
+
+
 
 
 
@@ -341,7 +341,7 @@ const teamData = {
 
 
 
-      
+
 //   })
 
 
@@ -357,8 +357,26 @@ const setRoster = () => {
   $.ajax({
     url: "/api/roster/" + teamAbbr,
     method: "GET"
-  }).then(function(data) {
-    console.log(data)
+  }).then(function (data) {
+    // console.log(data)
+    data.forEach(player => {
+      let $tableRow = $("<tr>");
+      let $jerseyNumber = $("<td>").text(player.jerseyNumber);
+      let $firstName = $("<td>").text(player.firstName);
+      let $lastName = $("<td>").text(player.lastName);
+      let $primaryPosition = $("<td>").text(player.primaryPosition);
+      let $height = $("<td>").text(player.height);
+      let $weight = $("<td>").text(player.weight);
+      let $age = $("<td>").text(player.age);
+
+      $tableRow.append($jerseyNumber, $firstName, $lastName, $primaryPosition, $height, $weight, $age)
+      $("#player-table").append($tableRow)
+      console.log($tableRow)
+    })
+
+
+
+
   })
 }
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 var MySportsFeeds = require("mysportsfeeds-node");
 var msf = new MySportsFeeds("2.0", true);
 msf.authenticate("3c05ee98-ad49-4e16-b24e-46c9b5", "MYSPORTSFEEDS");
-// var data = msf.getData('nfl', '2018-2019-regular', 'players', 'json', {rosterstatus: "assigned-to-roster"});
+var data = msf.getData('nfl', '2018-2019-regular', 'players', 'json', {sort: "player.position"}, {rosterstatus: 'assigned-to-roster'});
 // var data = msf.getData('nfl', '2018-2019-regular', 'weekly_games', 'json', {week: "12", sort: "game.starttime", rosterstatus: "assigned-to-roster", force: "true"});
 
 
@@ -82,7 +82,7 @@ app.get("/api/news/:team", function (req, res) {
   }).then((response, err) => {
     if (err) throw err;
 
-      res.json(response)
+    res.json(response)
 
   })
 })
@@ -91,69 +91,76 @@ app.get("/api/roster/:team", function (req, res) {
 
   console.log(req.params.team)
 
-// Reads the file that gets created with the data called from the API
-fs.readFile('results/players-nfl-2018-2019-regular.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  // const filteredData = 
-  console.log(JSON.parse(data, null, 2).players[0])
-    // console.log(player)
-    // == req.params.team;
-  // })
-  
-  // console.log(filteredData)
-  
-  
-  // .filter(player => {
-  
-  //   const playerData = {
-  //     "jerseyNumber": player.player.jerseyNumber,
-  //     "firstName": player.player.firstName,
-  //     "lastName": player.player.lastName,
-  //     "primaryPosition": player.player.primaryPosition,
-  //     "height": player.player.height,
-  //     "weight": player.player.weight,
-  //     "age": player.player.age,
-  //   } 
-  //   // res.json(playerData);
-  //   // playerData.forEach(player => console.log(isNullOrZero(player)));
-  // })
-});
+  // Reads the file that gets created with the data called from the API
+  fs.readFile('results/players-nfl-2018-2019-regular.json', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    const filteredData = JSON.parse(data, null, 2).players.filter(athlete => {
+
+    
+      return (athlete.player.currentTeam != null) && (athlete.player.currentTeam.abbreviation === req.params.team);
+      
+    });
+
+    const playerArray = [];
 
 
-// "player"."teamAsOfDate":{
-//   "id":67,
-//   "abbreviation":"TEN"
-  // }
+    filteredData.forEach(athlete => {
 
+      playerArray.push(
+        {
+          "jerseyNumber": athlete.player.jerseyNumber,
+          "firstName": athlete.player.firstName,
+          "lastName": athlete.player.lastName,
+          "primaryPosition": athlete.player.primaryPosition,
+          "height": athlete.player.height,
+          "weight": athlete.player.weight,
+          "age": athlete.player.age
+        }
+      );
+    
+    });
 
+    const nulledArray = replaceNull(playerArray);
+      
+    res.json(nulledArray);
+
+  })
 })
+  
 
 
 
-// NFL News API Call
-// newsapi.v2.everything({
-//   sources: "nfl-news",
-//   sortBy: 'publishedAt',
-//   pageSize: 7,
-//   language: 'en'
-// }).then((response, err) => {
-//   if (err) throw err;
+  // NFL News API Call
+  // newsapi.v2.everything({
+  //   sources: "nfl-news",
+  //   sortBy: 'publishedAt',
+  //   pageSize: 7,
+  //   language: 'en'
+  // }).then((response, err) => {
+  //   if (err) throw err;
 
-//   response.articles.forEach(article => {
-//     const nflArticleData = [article.source.name, article.publishedAt, article.url, article.urlToImage, article.title, article.description]
-//     nflArticleData.forEach(article => console.log(article))
-//   })
-// });
+  //   response.articles.forEach(article => {
+  //     const nflArticleData = [article.source.name, article.publishedAt, article.url, article.urlToImage, article.title, article.description]
+  //     nflArticleData.forEach(article => console.log(article))
+  //   })
+  // });
 
 
-// Function to replace data in Roster Chart not put in by API with a '*'. Will have a note on the site for this
-function isNullOrZero(data) {
-  return (data === null || data === 0) ? ('*') : (data);
-}
+  // Function to replace data in Roster Chart not put in by API with a '*'. Will have a note on the site for this
+  function replaceNull(data) {
 
-app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
-});
+    data.forEach(obj => {
+      for (let key in obj) {
+        (obj[key] === null) ? (obj[key] = '*') : (false);
+      }
+    })
+      return data;
+  }
+
+  app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+  });
 
 
 
